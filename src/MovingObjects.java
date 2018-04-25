8e4f41d693e028dc847a548e3e4cf35afb86208c
Jordan P <jordanep@udel.edu>
2018-04-25 13:08:21 -0400

    lkxz'l

--- src/MovingObjects.java
+++ src/MovingObjects.java
@@ -2,19 +2,24 @@
 public abstract class MovingObjects extends GameObject {
 	
 	boolean isMoving;
+	int currentX;
+	int currentY;
 	
 	protected MovingObjects(int currentX, int currentY, boolean isMoving) {
 		super(currentX, currentY);
+		this.currentX = currentX;
+		this.currentY = currentY;
 		this.isMoving = isMoving;
 	}
 	
 	protected boolean isCollided;
 	
-	protected void moveRight() {}
-	protected void moveLeft() {}
-	protected void moveDown() {}
-	protected void moreUp() {}
-	protected void die() {}
+	protected void moveRight() {currentX++;}
+	protected void moveLeft() {currentX--;}
+	protected void moveDown() {currentY--;}
+	protected void moreUp() {currentY++;}
+	protected void die() {} //test number of lives if more than 1 reset PacMan and ghosts to start but leave
+							//pellets and points and fruit
 	
 	protected void updatePos() {}
 
