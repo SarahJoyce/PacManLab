9cf83dfe42377f5953bc4797ba001474f86db296
SarahJoyce <sejoyce99@gmail.com>
2018-05-02 12:55:56 -0400

    more graphics

--- src/PacManGame.java
+++ src/PacManGame.java
@@ -1,17 +1,17 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class PacManGame {
+public class PacManGame extends JFrame{
+	
+	private JPanel contentPane;
 	
 	public static void main(String[] args) {
-		
-		/*JFrame window = new JFrame();
-		window.setBounds(0,0,500,500);
-		FlowLayout flow = new FlowLayout();
-		window.setLayout(flow);*/
-		//^might want to move this to world???^/
 		
 		PacMan player = new PacMan();
 		Ghost Inky = new Ghost();
@@ -20,7 +20,29 @@ public class PacManGame {
 		Ghost Clyde = new Ghost();
 	
 		World game = new World(player, Inky, Blinky, Pinky, Clyde);
-		game.draw();
-	}
 		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PacManGame window = new PacManGame();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+	
+	public PacManGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		FlowLayout flow = new FlowLayout();
+		contentPane.setLayout(flow);
+		setContentPane(contentPane);
+	}
+	
 }
