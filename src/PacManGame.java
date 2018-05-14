9140ab4aa7e783f921a6694aa1ca8684dad6a7f0
SarahJoyce <sejoyce99@gmail.com>
2018-05-14 19:20:22 -0400

    ok, NOW it's fixed

--- src/PacManGame.java
+++ src/PacManGame.java
@@ -9,15 +9,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 //primary game logic goes here
-public class PacManGame extends JFrame /*implements KeyListener*/{
+public class PacManGame extends JPanel /*implements KeyListener*/{
 	private static final long serialVersionUID = 1L;
-	
-	private JPanel contentPane;
 	
 	IEverythingGraphics graphicsEngine;
 	World game;
 
-	public PacManGame() {
+	public PacManGame(IEverythingGraphics graphicsEngine) {
 		this.graphicsEngine = graphicsEngine;
 		this.game = new World();
 	}
@@ -27,53 +25,11 @@ public class PacManGame extends JFrame /*implements KeyListener*/{
 		return new Dimension(225,225);
 	}
 
-	/*@Override
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		super.paintComponents(g);
 		graphicsEngine.draw(g, game);
-	}*/
-	
-	public static void main(String[] args) {
-		
-		PacMan player = new PacMan(7, 6, false, 0);
-		Ghost Inky = new Ghost(2, 11, false, false, GameObject.ObjectType.EMPTY);
-		Ghost Blinky = new Ghost(3, 11, false, false, GameObject.ObjectType.EMPTY);
-		Ghost Pinky = new Ghost(4, 11, false, false, GameObject.ObjectType.EMPTY);
-		Ghost Clyde = new Ghost(5, 11, false, false, GameObject.ObjectType.EMPTY);
-		
-		Random rand = new Random();
-		int inkyDirec;
-		int blinkyDirec;
-		int pinkyDirec;
-		int clydeDirec;
-		
-		//Do this every time there's a keystroke
-		inkyDirec = rand.nextInt(4);
-		Inky.nextPosn(inkyDirec);
-		blinkyDirec = rand.nextInt(4);
-		Blinky.nextPosn(blinkyDirec);
-		pinkyDirec = rand.nextInt(4);
-		Pinky.nextPosn(pinkyDirec);
-		clydeDirec = rand.nextInt(4);
-		Clyde.nextPosn(clydeDirec);
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PacManGame window = new PacManGame() {
-						@Override public void paintComponents(Graphics g) {
-							super.paintComponents(g);
-							graphicsEngine.draw(g, game);
-				          }
-					};
-					window.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
 	}
+	
 	
 	/*@Override
     public void keyPressed(KeyEvent e) {
